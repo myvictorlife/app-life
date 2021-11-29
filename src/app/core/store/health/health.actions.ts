15ccc43824e9c3fa04177cd4d7ec476976e6275f
Victor Cesar <victorcesar@Victors-MacBook-Pro.local>
2021-11-29 23:32:46 +0100

    feat: add unit test for effects

--- src/app/core/store/health/health.actions.ts
+++ src/app/core/store/health/health.actions.ts
@@ -2,7 +2,7 @@
  * File: health.actions.ts
  * Project: LIFE
  * Created: Saturday, 20th November 2021 11:43:54 pm
- * Last Modified: Friday, 26th November 2021 8:17:11 pm
+ * Last Modified: Monday, 29th November 2021 9:40:52 pm
  * Copyright © 2021 My Custom Life
  */
 
@@ -12,4 +12,5 @@ import { createAction, props } from '@ngrx/store';
 export const healthActions = {
   getInfo: createAction('[Health API] Get Health Info'),
   getInfoSuccess: createAction('[Health API] Health Loaded Success', props<{ health: Health }>()),
+  getInfoFailed: createAction('[Health API] Healths Failed'),
 };
--- src/app/core/store/health/health.effects.ts
+++ src/app/core/store/health/health.effects.ts
@@ -2,7 +2,7 @@
  * File: health.effects.ts
  * Project: LIFE
  * Created: Saturday, 20th November 2021 9:16:27 am
- * Last Modified: Wednesday, 24th November 2021 5:34:35 pm
+ * Last Modified: Monday, 29th November 2021 11:19:44 pm
  * Copyright © 2021 My Custom Life
  */
 
@@ -10,22 +10,18 @@ import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { HealthService } from '@life-service/health/health.service';
 import { healthActions } from '@life-store/health/health.actions';
-import { catchError, map, mergeMap } from 'rxjs/operators';
-import { EMPTY } from 'rxjs';
+import { catchError, exhaustMap, map } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 @Injectable()
 export class HealthEffects {
-  healthInfo$ = createEffect(() =>
-    /* eslint-disable-next-line */
+  healthInfo$ = createEffect(() => (
     this.actions$.pipe(
       ofType(healthActions.getInfo),
-      mergeMap(() =>
-        this.healthService.getInfo().pipe(
-          map((health) => healthActions.getInfoSuccess({ health })),
-          catchError(() => EMPTY),
-        ),
-      ),
-    ),
+      exhaustMap(() => this.healthService.getInfo()),
+      map((health) => healthActions.getInfoSuccess({ health })),
+      catchError(() => of(healthActions.getInfoFailed()))
+    )),
   );
 
   constructor(private actions$: Actions, private healthService: HealthService) {}
