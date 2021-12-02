206a0a81e2a61cc5929eb402dc7bd5fd32850168
Victor Cesar <victorcesar@Victors-MacBook-Pro.local>
2021-12-02 08:25:05 +0100

    fix: test

--- src/app/components/loading/loading/loading.component.ts
+++ src/app/components/loading/loading/loading.component.ts
@@ -2,7 +2,7 @@
  * File: loading.component.ts
  * Project: LIFE
  * Created: Tuesday, 30th November 2021 5:57:49 pm
- * Last Modified: Thursday, 2nd December 2021 8:23:24 am
+ * Last Modified: Thursday, 2nd December 2021 8:24:43 am
  * Copyright © 2021 My Custom Life
  */
 
@@ -25,8 +25,7 @@ export class LoadingComponent implements OnInit, OnDestroy {
   constructor(
     public loadingController: LoadingController,
     private store: Store
-  ) {
-  }
+  ) { }
 
   ngOnInit() {
     this.listenerLoading$ = this.store.select(selectIsLoadingSpinnerActive);
